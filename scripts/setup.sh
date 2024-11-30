6ad52af1caf1afe48b4eb5e7e740905895714655
pooriazln <pooria.zln83@gmail.com>
2024-11-30 17:16:42 +0330

    :sparkles: Add WG Config Manager

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,6 +16,8 @@ sudo apt-get install -y nodejs
 node -v
 npm -v
 
+npm install pm2 -g
+
 # Enable IPv4 forwarding for WireGuard
 echo "Enabling IPv4 forwarding..."
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
