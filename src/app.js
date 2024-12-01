37c2aa766fc9de7d3158766af2722409b6acb4c5
pooriazln <pooria.zln83@gmail.com>
2024-12-01 15:29:27 +0330

    :sparkles: Add WG Config Manager

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   try {
     await sequelize.authenticate();
-    await sequelize.sync();
+
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
