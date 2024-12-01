942bf557902960afefdb47843b58309093fbd520
pooriazln <pooria.zln83@gmail.com>
2024-12-01 15:27:20 +0330

    :sparkles: Add WG Config Manager

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require("sequelize");
 const path = require("path");
 
-export const sequelize = new Sequelize({
+const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: path.join(__dirname, "..", "data", "database.sqlite"),
 });
+module.exports = { sequelize };
