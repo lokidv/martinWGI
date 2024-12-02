c38667ae16fb3854793c289e6b2c70fd467753a1
pooriazln <pooria.zln83@gmail.com>
2024-12-02 12:37:52 +0330

    :sparkles: Add WG Config Manager

--- src/api.js
+++ src/api.js
@@ -39,11 +39,12 @@ router.get("/configs/:username", async (req, res) => {
   const config = await Config.findOne({ where: { username } });
   const publicIp = getPublicIp(); // Get public IP using local network interface
   const port = getPortFromConfig(); // Extract port from wg0.conf
+  const privateKey = fs.readFileSync("/etc/wireguard/private.key", "utf8");
 
   // Generate the WireGuard configuration content
   const configContent = `[Interface]
-PrivateKey = ${config.private_key}
-Address = ${config.allowed_ip}
+PrivateKey = ${privateKey}
+Address = ${config.allowed_ip}/24
 DNS = 1.1.1.1,1.0.0.1
 MTU = 1340
 
@@ -52,7 +53,10 @@ PublicKey = ${config.public_key}
 Endpoint = ${publicIp}:${port}
 AllowedIPs = 0.0.0.0`;
 
-  res.setHeader("Content-Disposition", `attachment; filename=${username}.conf`);
+  res.setHeader(
+    "Content-Disposition",
+    `attachment; filename=${username}_wg0.conf`
+  );
   res.setHeader("Content-Type", "text/plain");
   res.send(configContent);
 });
